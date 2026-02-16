70ef7cc865f5c74b5eebfc89b6d7b4a7b45829cb
Renato Gomes <renatotg10@gmail.com>
2026-02-16 12:22:24 -0300

    Dia 2: criação das rotas e layouts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+
 import { Home } from './features/home/home';
+import { List } from './features/list/list';
+import { Create } from './features/create/create';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: Home
-    }
+    { path: '', component: Home },
+    { path: 'list', component: List },
+    { path: 'create', component: Create },
+
+    // fallback (se digitar a rota errada)
+    { path: '**', redirectTo: '' },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('ferias-produtivas');
-}
+export class App {}
