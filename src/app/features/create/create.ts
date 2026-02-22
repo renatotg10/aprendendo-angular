c979dd9c883e13bfd7b5f2d6f0195276441217dd
Renato Gomes <renatotg10@gmail.com>
2026-02-22 10:55:36 -0300

    feat(create): implementar formulário reativo com validações (dia 3)

--- src/app/features/create/create.ts
+++ src/app/features/create/create.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './create.html',
   styleUrl: './create.css',
 })
 export class Create {
+  submitted = false;
+  form;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.nonNullable.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      age: [18, [Validators.required, Validators.min(0), Validators.max(120)]],
+    });
+  }
+
+  submit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log('FORM OK ✅', this.form.getRawValue());
+  }
 }
